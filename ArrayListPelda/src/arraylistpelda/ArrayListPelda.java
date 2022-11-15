765ebfbbb885bea5f9d9cbc77d1a0734ba1ee7bb
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2022-11-15 10:53:43 +0100

    Update ArrayListPelda.java

--- ArrayListPelda/src/arraylistpelda/ArrayListPelda.java
+++ ArrayListPelda/src/arraylistpelda/ArrayListPelda.java
@@ -5,6 +5,9 @@
  */
 package arraylistpelda;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author user3
@@ -15,7 +18,33 @@ public class ArrayListPelda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        ArrayList<Integer> szamok = new ArrayList<>();
+        
+        Random rand = new Random();
+        
+       // ArrayList feltöltése random számokkal        
+        for (int i = 0; i < 10; i++) {
+         int egySzam= rand.nextInt(10) + 10;
+         szamok.add(egySzam);
+        }
+        
+        // ArryaList kiíratás
+        for (int i = 0; i <szamok.size(); i++){
+            System.out.println(szamok.get(i));
+        }
+        
+        // Hány páros szám van a listában?
+        int paros = 0;
+        for (int i = 0; i < szamok.size(); i++){
+            if (szamok.get(i) % 2 == 0){
+                paros = paros + 1;
+            }
+        }
+        
+        System.out.println(paros + " páros szám van a liatában.");
+        
+        
     }
     
 }
