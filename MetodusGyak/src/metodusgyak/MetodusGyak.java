daa5e581f80cd7ecff691a914cb28cf661f4be77
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2022-11-29 13:09:17 +0100

    Update MetodusGyak.java

--- MetodusGyak/src/metodusgyak/MetodusGyak.java
+++ MetodusGyak/src/metodusgyak/MetodusGyak.java
@@ -10,12 +10,43 @@ package metodusgyak;
  * @author user3
  */
 public class MetodusGyak {
+    
+    public static String parose(int szam) {
+        String paros = "";
+        
+        for (){
+    
+        }
+        return paros;
+    }
+    
+     public static int kozepso(int szam1, int szam2, int szam3) {
+        int  kozepso = 0;
+        int min = Math.min(Math.min(szam1, szam2), szam3);
+        int max = Math.max(Math.max(szam1, szam2), szam3);
+        
+        return kozepso;
+    }
+     
+     public static int matrix(int n) {
+         int matrix = 0;
+         return matrix;
+     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 1. Ellenőrzi, hogy egy egész szám minden számjegye páros-e
+           //System.out.println("A szám " + parose(154));
+        
+        // 2. Három egész számot kér, és eldönti, hogy az egyik a másik kettő középpontja-e.
+           System.out.println(kozepso(8, 2, 5));
+        
+        // 3. Egy n számot kap paraméterként, és kiír egy nn x n-es mátrixot véletlen
+          // 0 illetve 1 számjegyekből
+          
+           System.out.println("");
     }
     
 }
