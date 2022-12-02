3a0574fec5e25efb7133f63ef517a004ac5b3021
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2022-12-02 13:21:28 +0100

    Update Main.java

--- 1202/src/pkg1202/Main.java
+++ 1202/src/pkg1202/Main.java
@@ -15,7 +15,28 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 1. Írj egy programot, ami megkeresi egy tömb 2. legnagyobb elemét.
+          int[] tomb = {5,17,4,7,55};
+        
+          for (int i = 0; i < tomb.length; i++) {
+           
+          }
+               
+        
+        // 2. Írj egy programot, ami összehasonlít két egyenlő méretű tömböt, 
+         // és megállapítja, hogy azonosak-e.
+          int[] tomb2 = {4,5,6,7};
+          int[] tomb3 = {4,5,6,7};
+          String azonose = "A két tömb azonos."; 
+          
+          for (int i = 0; i < tomb2.length; i++) {
+               if (tomb2[i] != tomb3[i]) {
+                   azonose = "A két tömb nem azonos";
+               }   
+          }
+          
+          System.out.println(azonose);
+         
     }
     
 }
