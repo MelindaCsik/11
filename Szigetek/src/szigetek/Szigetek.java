e14bdb2acff05da30c6a2e64a28713e69ec14a6c
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2023-01-10 12:51:15 +0100

    Create Szigetek.java

---
+++ Szigetek/src/szigetek/Szigetek.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package szigetek;
+/**
+ *
+ * @author user3
+ */
+public class Szigetek {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tomb[] = {0, 0, 200, 548, 20, 0, 0, 0, 310, 980, 1280, 650, 102, 0, 0, 45, 0, 21, 65, 0, 0};
+        
+        // Készíts programot, amely meghatározza a völgyet tartalmazó szigetek számát!
+        
+        
+        // Készíts programot, amely meghatározza az 200 méternél magasabb szigetek átlagos magasságát!
+           
+           
+           
+           for( int i = 0; i < tomb.length; i++ ){
+            if (tomb[i] > 200){
+                
+            }
+           }
+        
+        // Készíts programot, amely meg­határozza a 100 méternél alacsonyabb szigetek átlagos magasságát!
+          
+          for( int i = 0; i < tomb.length; i++ ){
+            if (tomb[i] < 100){
+                
+            }
+           }
+        
+        // Készíts programot, amely meghatározza a tengerszakaszok átlagos hosszát!
+        
+        
+    }
+}
+    
