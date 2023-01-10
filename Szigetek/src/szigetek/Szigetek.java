c14c1ae0f45c226d1b6f0ad9c1ee9bb9c8360285
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2023-01-10 13:23:53 +0100

    Update Szigetek.java

--- Szigetek/src/szigetek/Szigetek.java
+++ Szigetek/src/szigetek/Szigetek.java
@@ -19,22 +19,33 @@ public class Szigetek {
         
         
         // Készíts programot, amely meghatározza az 200 méternél magasabb szigetek átlagos magasságát!
-           
-           
+           int magash = 0;
+           int mennyim = 0;          
            
            for( int i = 0; i < tomb.length; i++ ){
             if (tomb[i] > 200){
-                
+                magash = magash + tomb[i];
+                mennyim = mennyim + 1;
             }
            }
+           
+           int magas = magash / mennyim;       
+           System.out.println("200 méternél magasbb szigetek átlagos magassága:" + magas);
+           
         
         // Készíts programot, amely meg­határozza a 100 méternél alacsonyabb szigetek átlagos magasságát!
+          int alacsonyh = 0;
+          int mennyia = 0;          
           
           for( int i = 0; i < tomb.length; i++ ){
             if (tomb[i] < 100){
-                
+                alacsonyh = alacsonyh + tomb[i];
+                mennyia = mennyia + 1;
             }
            }
+          
+          int alacsony = alacsonyh + mennyia;
+          System.out.println("100 méternél alacsonyabb");
         
         // Készíts programot, amely meghatározza a tengerszakaszok átlagos hosszát!
         
